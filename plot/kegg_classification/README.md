3f821fae34b77aa35dcd6ede9cf06cc62601f494
Your Name <you@example.com>
2017-07-18 16:33:25 +0800

    kegg class

---
+++ plot/kegg_classification/README.md
@@ -0,0 +1,9 @@
+## 功能
+### 读入KEGG claffification文件,输出条形图
+
+## 用法
+### Example
+`python3 kegg_classification.py -c class.txt`
+`python3 kegg_classification.py -c class.txt -s case_vs_control -o ./`
+## 输出文件示例
+![alt text](https://github.com/seqyuan/programs/tree/master/kegg_classification/sample_KEGG_Classification.png "Example")
---
+++ plot/kegg_classification/kegg_classification.py
@@ -0,0 +1,106 @@
+"""
+For KEGG Classification, outfile is sample_KEGG_Classification.pdf
+
+In file format:
+    Group   Classification  Value
+    Cell growth and death   A   109
+    Transport and catabolism    A   167
+    Cell motility   A   32
+    ...
+    ...
+
+Date 20170718
+"""
+import argparse
+import os
+import sys
+import re
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+Bin = os.path.abspath(os.path.dirname(__file__))
+
+__author__='Yuan Zan'
+__mail__= 'seqyuan@gmail.com'
+__date__= '20170718'
+
+class kegg_class:
+    df = None
+    legend_df = None
+    ax = None
+    maxvalue = None
+    N = None
+
+    def Init(self,class_file,ax,sample,outdir):
+        df = pd.read_table(class_file, header = 0, index_col=None, encoding='utf-8')
+        df =df[::-1]
+        df.reset_index(inplace=True,drop = True)
+
+        df['color'] = None
+        colors = ["b","g","r","c","m","y","b","g","r","c","m","y"]
+        df_copy = df.copy()
+        ii = -1
+        classi = None
+        for i,r in df_copy.iterrows():
+            if classi != r['Classification']:
+                ii += 1
+                classi = r['Classification']
+            df.ix[i,'color'] = colors[ii]
+
+        df['classNum'] = 1
+        df2 = df[['Classification','classNum','color']]
+
+        self.ax = ax
+        self.maxvalue = df['Value'].describe()['max']
+        self.df = df
+        self.legend_df = df2.groupby(['Classification','color'], axis=0).sum()[::-1]
+        self.N = self.df.shape[0]
+
+    def plot_barth(self):
+        self.ax.barh(bottom=range(self.N),width=self.df['Value'],height=0.8,left=0,color=self.df['color'],edgecolor='white',align='center',alpha=1)
+        for i,row in self.df.iterrows():
+            self.ax.text(row['Value'] + self.maxvalue*0.01, i, row['Value'], ha='left', va= 'center',fontsize=7)
+
+        self.ax.set_yticks([i for i in range(self.N)])
+        self.ax.set_yticklabels(self.df['Group'],rotation='horizontal',fontsize='x-small')
+        self.ax.set_xlabel('Number of Genes')
+        self.ax.tick_params(bottom ='on',top='off',left='off',right='off')
+
+    def plot_legend(self):
+        bottom = 0
+        for i,r in self.legend_df.iterrows():
+            self.ax.bar(left=[self.maxvalue*1.1], height=[r['classNum']-0.7], width=4, bottom=bottom,color=i[1],align="edge",edgecolor=i[1])
+            self.ax.text(self.maxvalue*1.14, bottom + (r['classNum'])/2, i[0], ha='left', va= 'center',fontsize=10)
+            bottom += r['classNum']
+
+
+def main():
+    parser=argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog='author:\t{0}\nmail:\t{1}\ndate:\t{2}\n'.format(__author__,__mail__,__date__))
+    parser.add_argument('-c','--classFile',help='class.txt',dest='classi',type=str,required=True)
+    parser.add_argument('-s','--sample',help='sample',type=str,default='sample')
+    parser.add_argument('-o','--outDir',help='outDir',dest='outDir',type=str,default=Bin)
+
+    args=parser.parse_args()
+
+    fig = plt.figure(figsize=(8,8))
+    [ax_x, ax_y, ax_w, ax_h] = [0.35,0.1,0.6,0.8]   #[0.05,0.07,0.07,0.66] 
+    ax = fig.add_axes([ax_x, ax_y, ax_w, ax_h], frame_on=True, axisbg = 'white')
+
+    kc = kegg_class()
+    kc.Init(args.classi,ax,args.sample,args.outDir)
+    kc.plot_barth()
+    kc.plot_legend()
+
+    ax.axhline(-1, color='k',linewidth=3)
+    ax.set_title('KEGG Classification')
+
+    fig.savefig(os.path.join(args.outDir,"{0}_KEGG_Classification.pdf".format(args.sample)))
+    fig.savefig(os.path.join(args.outDir,"{0}_KEGG_Classification.png".format(args.sample)))
+
+if __name__=="__main__": 
+    main()
