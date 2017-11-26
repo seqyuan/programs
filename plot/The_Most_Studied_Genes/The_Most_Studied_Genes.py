689bcdb7d079812593f3d67cd6f1e9b570e3500a
seqyuan <ahworld07@gmail.com>
2017-11-26 22:27:19 +0800

    top 20 complete

--- plot/The_Most_Studied_Genes/The_Most_Studied_Genes.py
+++ plot/The_Most_Studied_Genes/The_Most_Studied_Genes.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 usage:
+    bar plot the most sudied genes
     python3 ttt.py -i gene_info_total_human.tsv
 
 Created on Sat Nov 11 21:26 2017
@@ -16,7 +17,7 @@ import os
 import sys
 import pandas as pd
 import matplotlib as mpl
-#mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import warnings
@@ -24,29 +25,36 @@ warnings.filterwarnings("ignore")
 import argparse
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-def str_len_format(text, format_len):
-    text = text + (format_len - len(text) + 1) * '-'
-    return text
-
 def plot_barth(ax, df, N):
-    ax.barh(bottom=range(N),width=df['citations'],height=0.8,left=0,color='#00AEEF',edgecolor='#00AEEF',align='center',alpha=1)
-    max_text_len = 0
+    ax.barh(bottom=range(N),width=df['citations'],height=0.85,left=0,color='#00AEEF',edgecolor='#00AEEF',align='center',alpha=1)
     for i,row in df.iterrows():
-        text = row['citations']
+        text = format(row['citations'],',')
         if i == 0:
             text = '{} citations'.format(text)
-        ax.text(2, i, text, ha='left', va= 'center',fontsize=7,color='w')
-        if len('{0} {1}'.format(i+1,row['gene'])) > max_text_len:
-            max_text_len = len('{0} {1}'.format(i+1,row['gene']))
-    
+        ax.text(100, i, text, ha='left', va= 'center',fontsize=7,color='w')
+   
     ax.set_xticks([])
-    ax.set_yticks([i for i in range(N)])
-    ylabel = [str_len_format('{0} {1}'.format(i+1,row['gene']),max_text_len) for i,row in df.iterrows()]
-    for i in ylabel:
-        print (i)
-    ax.set_yticklabels(ylabel,color='#00AEEF',horizontalalignment='left',verticalalignment='center')
+    ax.set_yticklabels([])
     ax.tick_params(bottom ='off',top='off',left='off',right='off')
     ax.set_ylim([N+1,-1])
+
+def plot_title(df,ax):
+    t = 'THE TOP {}'.format(df.shape[0])
+    tt = 'The {0} most studied genes of all time are described in more than {1} papers.'.format(df.shape[0],format(df['citations'].sum(),','))
+
+    ax.text(0, 2, t, ha='left', va= 'center',fontsize=17,color='y',fontweight='bold')
+    ax.text(0, 0, tt, ha='left', va= 'center',fontsize=8,color='#00AEEF')
+    ax.set_ylim([-1,2])
+    ax.tick_params(bottom ='off',top='off',left='off',right='off')
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+def plot_ylabel(df,ax):
+    for i,row in df.iterrows():
+        ax.text(0, i, '{0} {1}'.format(i+1,row['gene']), ha='left', va= 'center',fontsize=9,color='#00AEEF')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_ylim([df.shape[0]+1,-1])
 
 def main():
     parser=argparse.ArgumentParser(description=__doc__,
@@ -61,14 +69,26 @@ def main():
     df.index = list(range(df.shape[0]))
     df = df[['gene','describe','citations']].ix[0:args.topNum-1,:]
 
-    fig = plt.figure(figsize=(6,8))
-    [ax_x, ax_y, ax_w, ax_h] = [0.4,0.1,0.5,0.7]
+    h = 3
+    if args.topNum > 10:
+        h = int(args.topNum/10) * h
+
+    fig = plt.figure(figsize=(6,h))
+
+    [ax_x, ax_y, ax_w, ax_h] = [0.3,0.1,0.6,0.7]
     ax_barh = fig.add_axes([ax_x, ax_y, ax_w, ax_h], frame_on=False, axisbg = '#231F20')
     plot_barth(ax_barh, df, args.topNum)
 
+    [ax1_x, ax1_y, ax1_w, ax1_h] = [0.1,0.1,0.2,0.7]
+    ax1 = fig.add_axes([ax1_x, ax1_y, ax1_w, ax1_h], frame_on=False, axisbg = '#231F20')
+    plot_ylabel(df, ax1)
 
+    [ax2_x, ax2_y, ax2_w, ax2_h] = [0.1,0.8,0.7,0.1]
+    ax2 = fig.add_axes([ax2_x, ax2_y, ax2_w, ax2_h], frame_on=False, axisbg = '#231F20')
+    plot_title(df, ax2)
 
-    plt.show()
+    #plt.show()
+    fig.savefig(args.outfile)
 
 if __name__ == '__main__':
     main()
