43e3afbf42ee72ed141180676bddfc7ca51504d5
Your Name <you@example.com>
2017-11-29 19:06:23 +0800

    la1

--- plot/The_Most_Studied_Genes/gene_on_chrom.py
+++ plot/The_Most_Studied_Genes/gene_on_chrom.py
@@ -5,7 +5,7 @@ usage:
     plot genes on chromosome
     python3 g.pene_on_chrom.py -i gene_info_total_human.tsv
 
-Created on Sun Nov 11 23:23 2017
+Created on Sun Nov 12 23:23 2017
 author: Zan Yuan
 email: seqyuan@gmail.com
 github: github.com/seqyuan
@@ -58,7 +58,7 @@ class chrom_class:
             ax.add_patch(left_Rectangle)
             ax.add_patch(right_Rectangle)
             ax.add_patch(cent_Rectangle)
-            ax.text(0, (i*10+6)*self.pad, row['chrom'].lstrip('chr'), ha='left', va= 'center',fontsize=18,color='y',fontweight='bold')
+            ax.text(0, (i*10+6)*self.pad, row['chrom'].lstrip('chr'), ha='left', va= 'center',fontsize=16,color='y',fontweight='bold')
 
             chrom_df = self.df_gene_citation_counts[self.df_gene_citation_counts['chrom'] == row['chrom']] 
             width = list(chrom_df['end'] - chrom_df['start'])
@@ -66,7 +66,13 @@ class chrom_class:
             height=chrom_df['citations']*7.5*self.pad
             left=chrom_df['start'] + self.pad
             ax.bar(left=left, height=height, width=width, bottom=bottom,color='y',edgecolor='y',align='edge',alpha=1)
-            
+   
+            if row['chrom'] in ['chrY','chr5']:
+                ax.text(row['length'] + 6 * self.pad, i*10*self.pad, 'Number of studies\ndescribing each gene', ha='left', va= 'bottom',fontsize=16,color='k',fontweight='normal')
+
+
+
+
         ax.set_yticks([])
         ax.set_yticklabels([])
         ax.set_xticks([])
@@ -86,20 +92,45 @@ class chrom_class:
             va= 'bottom'
             text = str(row['Ranking']) + ' ' + row['name']
             if row['citations'] == 1:
-                 y = iii*10*self.pad + row['citations']*7*self.pad
+                 y = iii*10*self.pad + row['citations']*6*self.pad
             elif row['citations'] == 8:
-                x = row['start'] - self.pad
+                x = row['start'] - 10*self.pad
                 y = iii*10*self.pad + row['citations']*7.6*self.pad
-                ha = 'right'
+                ha = 'left'
             
-            ax.text(x, y, text, ha=ha, va= va,fontsize=10,color='#00AEEF',fontweight='normal')
+            ax.text(x, y, text, ha=ha, va= va,fontsize=10,color='y',fontweight='normal')
+
+def plot_text(ax):
+    text = """
+In 2002, the US National Library of Medicine (NLM) began annotating papers in its
+popular PubMed database of biomedical literature. Articles are tagged if they contain
+information about the structure, function or location of a specific gene or gene product.
+The effort has recorded 1.2 million descriptions of 27,000 human genes - including RNA
+genes and pseudogenes - in about 565,000 articles. These data reveal trends in
+genetics research, as well as the list of most studied human genes. 
+    """
+    ax.text(0, 0, text, ha='left', va= 'bottom',fontsize=15,color='k',fontweight='normal')
+    ax.set_yticks([])
+    ax.set_yticklabels([])
+    ax.set_xticks([])
+    ax.set_xticklabels([])
+    ax.set_ylim([0,0.5])
+    ax.set_ylim([0,0.85])
+    ax.tick_params(bottom ='off',top='off',left='off',right='off')
+
+def plot_title(ax):
+    ax.text(0, 0, 'TOP GENES', ha='left', fontsize='40',color='y',fontweight='bold')
+    ax.set_yticks([])
+    ax.set_yticklabels([])
+    ax.set_xticks([])
+    ax.set_xticklabels([])
+    ax.tick_params(bottom ='off',top='off',left='off',right='off')
 
 def main(args):
     all_gene_counts_file, chromInfo, cytoBand = args
-    fig = plt.figure(figsize=(12,16),facecolor='k')
+    fig = plt.figure(figsize=(9.5,19),facecolor='k')
     #fig.patch.set_facecolor('black')
-
-    [ax_x, ax_y, ax_w, ax_h] = [0.05,0.03,0.8,0.9]
+    [ax_x, ax_y, ax_w, ax_h] = [0.05,0.03,0.9,0.79]
     ax_chrom = fig.add_axes([ax_x, ax_y, ax_w, ax_h], frame_on=False, axisbg = 'k')
     
     chrom = chrom_class()
@@ -108,9 +139,17 @@ def main(args):
     chrom.plot_top_10(ax_chrom)
     ax_chrom.set_title('<---  Gene position on the chromosome  --->')
 
+    [ax_x1, ax_y1, ax_w1, ax_h1] = [0.05,0.84,0.9,0.08]
+    ax_text = fig.add_axes([ax_x1, ax_y1, ax_w1, ax_h1], frame_on=False, axisbg = 'k')
+    plot_text(ax_text)
 
-    #plt.show()
+    [ax_x2, ax_y2, ax_w2, ax_h2] = [0.05,0.94,0.9,0.05]
+    ax_title = fig.add_axes([ax_x2, ax_y2, ax_w2, ax_h2], frame_on=False, axisbg = 'k')
+    plot_title(ax_title)
+
+    plt.show()
     fig.savefig('gene_on_chrom.pdf')
+    fig.savefig('gene_on_chrom.png')
 
 if __name__ == '__main__':
     args = ['all_gene_counts.tsv','chromInfo.txt','cytoBand.txt']
