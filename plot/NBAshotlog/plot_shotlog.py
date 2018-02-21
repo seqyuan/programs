e15e26659a8874d39f45ca03d01488deed314e3a
seqyuan <ahworld07@gmail.com>
2018-02-22 03:02:05 +0800

    court

--- plot/NBAshotlog/plot_shotlog.py
+++ plot/NBAshotlog/plot_shotlog.py
@@ -97,27 +97,83 @@ def draw_court(ax=None, color='black', lw=2, outer_lines=False):
 
     return ax
 
+
+class data_linewidth_plot():
+    def __init__(self,**kwargs):
+        self.ax = kwargs.pop("ax", plt.gca())
+        self.lw_data = kwargs.pop("linewidth", 1)
+        self.lw = 1
+        self.ax.figure.canvas.draw()
+        self.ppd=72./fig.dpi
+        self.trans = self.ax.transData.transform
+        self._resize()
+
+    def _resize(self):
+        lw =  ((self.trans((1,self.lw_data))-self.trans((0,0)))*self.ppd)[1]
+        self.lw = lw
+
 def draw_half_court(ax=None, unit=1, lineColor='white', courtColor='#F7CEA2'):
     if ax is None:
         ax = plt.gca()
 
     lw = unit * 2 #line width
-    # Create the basketball hoop
+    color = 'k'
+
+    # 精确line width
+    court_lw = data_linewidth_plot(ax = ax,linewidth = 1).lw
+
+    ## Create the basketball hoop
     #篮筐直径(内径)是18IN.,我们设置半径为9.2IN刨除line width 0.2IN,正好为篮筐半径
     hoop = Wedge((0, 0), unit * 9.2, 0, 360, width=unit * 0.2, color='#767676')
-    hoop_neck = Rectangle((unit * -48, unit * - ), unit * 6, unit * 6, linewidth=None, color='#767676')
-    # Create backboard
-    #Rectangle, lower left at xy = (x, y) with specified width, height and rotation angle
+    hoop_neck = Rectangle((unit * -2, unit * -15 ), unit * 4, unit * 6, linewidth=None, color='#767676')
+    
+	## Create backboard
+    #Rectangle, left lower at xy = (x, y) with specified width, height and rotation angle
     backboard = Rectangle((unit * -36, unit * -15 ), unit * 72, lw, linewidth=None, color='#767676')
     # List of the court elements to be plotted onto the axes
 
-    # Restricted Zone, it is an arc with 4ft radius from center of the hoop 
-    restricted = Wedge((0, 0), unit * 50, 0, 180, width=lw, color='#767676')
-    restricted_left = Rectangle((0, unit * -48 ), lw, unit * 15, linewidth=None, color='#767676')
-    restricted_right = Rectangle((0, unit * 48 ), lw, unit * 15, linewidth=None, color='#767676')
+    ## Restricted Zone, it is an arc with 4ft radius from center of the hoop 
+    #restricted = Wedge((0, 0), unit * 50, 0, 180, width=lw, color='#767676')
+    restricted = Arc((0, 0), 96+lw, 96+lw, theta1=0, theta2=180,linewidth=court_lw, color='#767676', fill=False)
+    #restricted_left = Rectangle((unit * -50, unit * -15 ), lw, unit * 15, linewidth=0, color='#767676')
+    restricted_left = Rectangle((-48*unit-lw/2, unit * -15 ), 0, unit * 17, linewidth=court_lw, color='#767676')
+    #restricted_right = Rectangle((unit * 48, unit * -15 ), lw, unit * 15, linewidth=0, color='#767676')
+    restricted_right = Rectangle((unit*48+lw/2, unit * -15 ), 0, unit * 17, linewidth=court_lw, color='#767676')
+
+    # Create free throw top arc  罚球线弧顶
+    top_arc_diameter = 6 * 12 * 2 - lw
+    top_free_throw = Arc((0, unit * 164), top_arc_diameter, top_arc_diameter, theta1=0, theta2=180,linewidth=court_lw, color=color, fill=False)
+    # Create free throw bottom arc 罚球底弧
+    bottom_free_throw = Arc((0, unit * 164), top_arc_diameter, top_arc_diameter, theta1=180, theta2=0,linewidth=court_lw, color=color, linestyle='dashed')
+
+    # Create the outer box 0f the paint, width=16ft outside , height=18ft 10in
+    outer_box = Rectangle((lw/2 - unit*96, -lw/2 - unit*63), 192-lw, 230-lw, linewidth=court_lw, color=color, fill=False)
+    # Create the inner box of the paint, widt=12ft, height=height=18ft 10in
+    inner_box = Rectangle((lw/2 - unit*72, -lw/2 - unit*63), 144-lw, 230-lw, linewidth=court_lw, color=color, fill=False)
+
+    ## Three point line
+    # Create the side 3pt lines, they are 14ft long before they begin to arc
+    #corner_three_left = Rectangle((-264*unit, -63*unit), lw, 168*unit, linewidth=0, color=color)
+    corner_three_left = Rectangle((-264*unit+lw/2, -63*unit-lw/2), 0, 169*unit, linewidth=court_lw, color=color)
+    corner_three_right = Rectangle((264*unit-lw/2, -63*unit-lw/2), 0, 169*unit, linewidth=court_lw, color=color)
+    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    # I just played around with the theta values until they lined up with the 
+    # threes 
+    three_diameter = (23 * 12 + 9) * 2 - lw
+    three_arc = Arc((0, 0), three_diameter, three_diameter, theta1=22, theta2=158, linewidth=court_lw, color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, (94*12/2-63)*unit), 48*unit+lw, 48*unit+lw, theta1=180, theta2=0,linewidth=court_lw, color=color)
+    center_inner_arc = Arc((0, (94*12/2-63)*unit), 144*unit-lw, 144*unit-lw, theta1=180, theta2=0,linewidth=court_lw, color=color)
+
+    # Draw the half court line, baseline and side out bound lines
+    outer_lines = Rectangle((-25*12*unit - lw/2, -63*unit-lw/2), 50*12*unit+lw, 94/2*12*unit + lw, linewidth=court_lw, color=color, fill=False)
+
+    #2 IN. WIDE BY 3 FT. DEEP, 28 FT. INSIDE, 3FT. extenf onto court
 
     court_elements = [hoop, hoop_neck, backboard, restricted, restricted_left, restricted_right,
-                        ]
+                        top_free_throw,bottom_free_throw,outer_box,inner_box,corner_three_left,corner_three_right,
+                        three_arc,center_outer_arc,center_inner_arc,outer_lines]
 
     # Add the court elements onto the axes
     for element in court_elements:
@@ -125,21 +181,21 @@ def draw_half_court(ax=None, unit=1, lineColor='white', courtColor='#F7CEA2'):
 
     #地板颜色'#F7CEA2'
 
-fig = plt.figure(figsize=(7,7))
+fig = plt.figure(figsize=(10,10))
 plt.axes().set_aspect('equal')
 
 ax = fig.add_subplot(111)
 
 
-
+ax.set_xlim(-330,330)
+ax.set_ylim(-200,600)
 
 draw_half_court(ax=ax)
 
 #ax.plot([7.5, 7.5], [-7.5, 7.5], 'k-', lw=2)
 #ax.plot([5.5, 5.5], [-7.5, 7.5], 'k-', lw=2)
 
-ax.set_xlim(-300,300)
-ax.set_ylim(-100,500)
+
 '''
 draw_court(ax=ax,outer_lines=False)
 plt.xlim(-300,300)
--- plot/NBAshotlog/readme.md
+++ plot/NBAshotlog/readme.md
@@ -29,7 +29,7 @@ NBA官方并没有提供公共的API方便我们访问球员的shot log, [Web Sc
 1. **锁定目标网站**
 
 目标网站：[stats.nba.com](http://stats.nba.com)
-
+\
 2. **具体网页对象**
 
 shot log所在的页面标签可能会有改变，有时不在很显眼的位置，这也是很多教程失效的原因(只给了最后API的网址，没有说这个网址是怎么来的)，所以这个得花时间找一下。
@@ -176,10 +176,113 @@ plt.show()
 
 ## 画篮球半场图
 
-通过对`LOC_X`和`LOC_Y`数据的快速画图，我们大概知道了篮筐的位置大概就是`LOC_X`和`LOC_Y`的原点。知道了这一点，我们结合篮球半场的具体尺寸 ([下图](https://www.sportsknowhow.com/basketball/dimensions/nba-basketball-court-dimensions.html))和比例就可以画出篮球半场图了。
+通过对`LOC_X`和`LOC_Y`数据的快速画图，我们大概知道了篮筐的位置大概就是`LOC_X`和`LOC_Y`的原点。知道了这一点，我们结合篮球半场的具体[尺寸](court_size.gif) ([下图](https://www.sportsknowhow.com/basketball/dimensions/nba-basketball-court-dimensions.html))和比例就可以画出篮球半场图了。
 
 [![nba_court_dimensions_h](nba_court_dimensions_h.png "")](http://www.sportscourtdimensions.com/wp-content/uploads/2015/02/nba_court_dimensions_h.png)
 
 通过上图我们知道了篮球场宽度是`50FT`，转换成`INCH`是`600IN`，篮球场长`94FT`，转换成`INCH`是`1128IN`，再结合我们上一步画出的投篮点快速预览图，我们能够推测出`LOC_X`和`LOC_Y`的计算单位是`IN`。
 
+画图函数如下：
 
+```
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle, Arc, Wedge
+
+
+class data_linewidth_plot():
+    def __init__(self,**kwargs):
+        self.ax = kwargs.pop("ax", plt.gca())
+        self.lw_data = kwargs.pop("linewidth", 1)
+        self.lw = 1
+        self.ax.figure.canvas.draw()
+        self.ppd=72./fig.dpi
+        self.trans = self.ax.transData.transform
+        self._resize()
+
+    def _resize(self):
+        lw =  ((self.trans((1,self.lw_data))-self.trans((0,0)))*self.ppd)[1]
+        self.lw = lw
+
+def draw_half_court(ax=None, unit=1, lineColor='white', courtColor='#F7CEA2'):
+    if ax is None:
+        ax = plt.gca()
+
+    lw = unit * 2 #line width
+    color = 'k'
+
+    # 精确line width
+    court_lw = data_linewidth_plot(ax = ax,linewidth = 1).lw
+
+    ## Create the basketball hoop
+    #篮筐直径(内径)是18IN.,我们设置半径为9.2IN刨除line width 0.2IN,正好为篮筐半径
+    hoop = Wedge((0, 0), unit * 9.2, 0, 360, width=unit * 0.2, color='#767676')
+    hoop_neck = Rectangle((unit * -2, unit * -15 ), unit * 4, unit * 6, linewidth=None, color='#767676')
+    
+	## Create backboard
+    #Rectangle, left lower at xy = (x, y) with specified width, height and rotation angle
+    backboard = Rectangle((unit * -36, unit * -15 ), unit * 72, lw, linewidth=None, color='#767676')
+    # List of the court elements to be plotted onto the axes
+
+    ## Restricted Zone, it is an arc with 4ft radius from center of the hoop 
+    restricted = Arc((0, 0), 96+lw, 96+lw, theta1=0, theta2=180,linewidth=court_lw, color='#767676', fill=False)
+    restricted_left = Rectangle((-48*unit-lw/2, unit * -15 ), 0, unit * 17, linewidth=court_lw, color='#767676')
+    restricted_right = Rectangle((unit*48+lw/2, unit * -15 ), 0, unit * 17, linewidth=court_lw, color='#767676')
+
+    # Create free throw top arc  罚球线弧顶
+    top_arc_diameter = 6 * 12 * 2 - lw
+    top_free_throw = Arc((0, unit * 164), top_arc_diameter, top_arc_diameter, theta1=0, theta2=180,linewidth=court_lw, color=color, fill=False)
+    # Create free throw bottom arc 罚球底弧
+    bottom_free_throw = Arc((0, unit * 164), top_arc_diameter, top_arc_diameter, theta1=180, theta2=0,linewidth=court_lw, color=color, linestyle='dashed')
+
+    # Create the outer box 0f the paint, width=16ft outside , height=18ft 10in
+    outer_box = Rectangle((lw/2 - unit*96, -lw/2 - unit*63), 192-lw, 230-lw, linewidth=court_lw, color=color, fill=False)
+    # Create the inner box of the paint, widt=12ft, height=height=18ft 10in
+    inner_box = Rectangle((lw/2 - unit*72, -lw/2 - unit*63), 144-lw, 230-lw, linewidth=court_lw, color=color, fill=False)
+
+    ## Three point line
+    # Create the side 3pt lines, they are 14ft long before they begin to arc
+    corner_three_left = Rectangle((-264*unit+lw/2, -63*unit-lw/2), 0, 169*unit, linewidth=court_lw, color=color)
+    corner_three_right = Rectangle((264*unit-lw/2, -63*unit-lw/2), 0, 169*unit, linewidth=court_lw, color=color)
+    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    # I just played around with the theta values until they lined up with the 
+    # threes 
+    three_diameter = (23 * 12 + 9) * 2 - lw
+    three_arc = Arc((0, 0), three_diameter, three_diameter, theta1=22, theta2=158, linewidth=court_lw, color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, (94*12/2-63)*unit), 48*unit+lw, 48*unit+lw, theta1=180, theta2=0,linewidth=court_lw, color=color)
+    center_inner_arc = Arc((0, (94*12/2-63)*unit), 144*unit-lw, 144*unit-lw, theta1=180, theta2=0,linewidth=court_lw, color=color)
+
+    # Draw the half court line, baseline and side out bound lines
+    outer_lines = Rectangle((-25*12*unit - lw/2, -63*unit-lw/2), 50*12*unit+lw, 94/2*12*unit + lw, linewidth=court_lw, color=color, fill=False)
+
+    #2 IN. WIDE BY 3 FT. DEEP, 28 FT. INSIDE, 3FT. extenf onto court
+
+    court_elements = [hoop, hoop_neck, backboard, restricted, restricted_left, restricted_right,
+                        top_free_throw,bottom_free_throw,outer_box,inner_box,corner_three_left,corner_three_right,
+                        three_arc,center_outer_arc,center_inner_arc,outer_lines]
+
+    # Add the court elements onto the axes
+    for element in court_elements:
+        ax.add_patch(element)
+
+```
+
+画图：
+
+```
+fig = plt.figure(figsize=(9,9))
+ax = fig.add_subplot(111,aspect='equal')
+ax.set_xlim(-330,330)
+ax.set_ylim(-200,600)
+
+draw_half_court(ax=ax)
+
+plt.show()
+```
+
+![court](court.png)
+
+添加上投篮数据
+
+![court](court.png)
